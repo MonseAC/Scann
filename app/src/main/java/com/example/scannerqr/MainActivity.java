d4afc86e063816b2e8f378f3f449cd626ca52ded
Monse <monse.tobi@gmail.com>
2019-05-19 01:44:33 -0600

    Boton con codigo y todavia nada

--- app/src/main/java/com/example/scannerqr/MainActivity.java
+++ app/src/main/java/com/example/scannerqr/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.scannerqr;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -9,9 +11,12 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        final Activity activity = this;
     }
 }
