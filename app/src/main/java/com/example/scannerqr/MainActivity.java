3095610b5f3946617d9f024b80b11be546ade5cb
Monse <monse.tobi@gmail.com>
2019-05-19 01:29:37 -0600

    No son los imports

--- app/src/main/java/com/example/scannerqr/MainActivity.java
+++ app/src/main/java/com/example/scannerqr/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.scannerqr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+} 
